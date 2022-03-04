a20c9e5d2f40629266150f60009c90679d0127ab
Solomon Sanderson <solomonsanderson@outlook.com>
2022-03-04 12:37:56 +0000

    Week 5

--- fit_MCZ0.py
+++ fit_MCZ0.py
@@ -12,12 +12,9 @@ from scipy.special import voigt_profile
 from load import load_data
 
 
-path = r"Code\data\MC-Z0.data"
-effmass = load_data(path)[6]
-fig, ax = plt.subplots()
-
-
-n, bins, patches = ax.hist(effmass, bins = 100, range = [40,140], density=True, alpha=0.3, label="Histogram of MC-Z0")
+def power_series(x, a, k, x0):
+    y = a * (x + x0)**(-k)
+    return y 
 
 def V(x, alpha, gamma, x0):
     """
@@ -29,29 +26,29 @@ def V(x, alpha, gamma, x0):
 
     return np.real(wofz((x - x0 + 1j*gamma)/sigma/np.sqrt(2))) / sigma /np.sqrt(2*np.pi)
 
-
-bins = bins[:-1]
-popt, pcov = curve_fit(V, bins, n,  p0=[1.35105, 2.20259, 90.25890], maxfev = 7000)
-alpha, gamma, x0 = popt[0], popt[1], popt[2]
-ax.plot(bins + 0.5, V(bins, alpha, gamma, x0), label=f"Voigt Fit Curve:\n $\\alpha={alpha:.5f}$ \n $\\gamma={gamma:.5f}$ \n $x_0 = {x0:.5f}$", color="hotpink"    )
-ax.set_xlabel('Invariant $\mu^{+} \mu^{-}$ mass (GeV/c$^{2}$)')
-ax.set_ylabel("Number")
-ax.set_title("Histogram of MC-Z0 data with Power Curve")
+if __name__ == "__main__":
+    path = r"Code\data\MC-Z0.data"
+    effmass = load_data(path)[6]
+    fig, ax = plt.subplots()
 
 
-def power_series(x, a, k, x0):
-    y = a * (x + x0)**(-k)
-    return y 
+    n, bins, patches = ax.hist(effmass, bins = 100, range = [40,140], density=False, alpha=0.3, label="Histogram of MC-Z0")
+
+    bins = bins[:-1]
+    popt, pcov = curve_fit(V, bins, n,  p0=[1.35105, 2.20259, 90.25890], maxfev = 7000)
+    alpha, gamma, x0 = popt[0], popt[1], popt[2]
+    ax.plot(bins + 0.5, V(bins, alpha, gamma, x0), label=f"Voigt Fit Curve:\n $\\alpha={alpha:.5f}$ \n $\\gamma={gamma:.5f}$ \n $x_0 = {x0:.5f}$", color="hotpink"    )
+    ax.set_xlabel('Invariant $\mu^{+} \mu^{-}$ mass (GeV/c$^{2}$)')
+    ax.set_ylabel("Number")
+    ax.set_title("Histogram of MC-Z0 data with Power Curve")
+
+    bins = bins[54:]
+    n = n[54:]
+
+    popt_power, pcov_power = curve_fit(power_series, bins, n, maxfev = 1000000)
+    a, k, x0 = popt_power[0], popt_power[1], popt_power[2]
+    ax.plot(bins+0.5, power_series(bins, a, k, x0),label=f"Power Fit Curve:\n $a={a:.5f}$ \n $k={k:.5f}$ \n $x_0 = {x0:.5f}$" , color="forestgreen", marker = None)
 
 
-bins = bins[89:]
-n = n[89:]
-
-
-popt_power, pcov_power = curve_fit(power_series, bins, n, maxfev = 1000000)
-a, k, x0 = popt_power[0], popt_power[1], popt_power[2]
-ax.plot(bins+0.5, power_series(bins, a, k, x0),label=f"Power Fit Curve:\n $a={a:.5f}$ \n $k={k:.5f}$ \n $x_0 = {x0:.5f}$" , color="forestgreen", marker = None)
-
-
-ax.legend()
-plt.show()
+    ax.legend()
+    plt.show()  
